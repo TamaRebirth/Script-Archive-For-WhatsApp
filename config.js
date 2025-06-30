abbc8fc68b5d96975548f191b8952a500aa41df8
TamaSmilikitiw <hendy3858@gmail.com>
2025-06-30 14:10:13 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7138646953:AAG1RdWSLXUMrARk0PtsKZSmd3YZaw96wDw",
+  CHANNEL_ID: "@aboutmetamarebirth",
+  SOURCE_URL: "https://www.gamersberg.com/grow-a-garden/stock"
+};
